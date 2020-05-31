0dcae161b87622cfd032d1d04051c9815951c0d0
Cristian Iorga <ubik3000@gmail.com>
2020-05-31 11:19:43 +0300

    WoL: Added more complete Network example

--- WOL/main.swift
+++ WOL/main.swift
@@ -6,6 +6,9 @@
 //  Copyright © 2020 Zenio. All rights reserved.
 //
 
+//TODO: Change port number for UDP packet (default and as argument)
+
+
 import Foundation
 import Network
 
@@ -26,6 +29,7 @@ for argument in CommandLine.arguments {
     }
 }
 
+//TODO: Add class for embedding connection
 var connection: NWConnection?
 
 func send() {
@@ -65,3 +69,83 @@ func someFunc() {
     connection?.start(queue: .global())
 
 }
+
+//class ViewController: UIViewController {
+//
+//    var connection: NWConnection?
+//    var hostUDP: NWEndpoint.Host = "iperf.volia.net"
+//    var portUDP: NWEndpoint.Port = 5201
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        // Hack to wait until everything is set up
+//        var x = 0
+//        while(x<1000000000) {
+//            x+=1
+//        }
+//        connectToUDP(hostUDP,portUDP)
+//    }
+//
+//    func connectToUDP(_ hostUDP: NWEndpoint.Host, _ portUDP: NWEndpoint.Port) {
+//        // Transmited message:
+//        let messageToUDP = "Test message"
+//
+//        self.connection = NWConnection(host: hostUDP, port: portUDP, using: .udp)
+//
+//        self.connection?.stateUpdateHandler = { (newState) in
+//            print("This is stateUpdateHandler:")
+//            switch (newState) {
+//                case .ready:
+//                    print("State: Ready\n")
+//                    self.sendUDP(messageToUDP)
+//                    self.receiveUDP()
+//                case .setup:
+//                    print("State: Setup\n")
+//                case .cancelled:
+//                    print("State: Cancelled\n")
+//                case .preparing:
+//                    print("State: Preparing\n")
+//                default:
+//                    print("ERROR! State not defined!\n")
+//            }
+//        }
+//
+//        self.connection?.start(queue: .global())
+//    }
+//
+//    func sendUDP(_ content: Data) {
+//        self.connection?.send(content: content, completion: NWConnection.SendCompletion.contentProcessed(({ (NWError) in
+//            if (NWError == nil) {
+//                print("Data was sent to UDP")
+//            } else {
+//                print("ERROR! Error when data (Type: Data) sending. NWError: \n \(NWError!)")
+//            }
+//        })))
+//    }
+//
+//    func sendUDP(_ content: String) {
+//        let contentToSendUDP = content.data(using: String.Encoding.utf8)
+//        self.connection?.send(content: contentToSendUDP, completion: NWConnection.SendCompletion.contentProcessed(({ (NWError) in
+//            if (NWError == nil) {
+//                print("Data was sent to UDP")
+//            } else {
+//                print("ERROR! Error when data (Type: Data) sending. NWError: \n \(NWError!)")
+//            }
+//        })))
+//    }
+//
+//    func receiveUDP() {
+//        self.connection?.receiveMessage { (data, context, isComplete, error) in
+//            if (isComplete) {
+//                print("Receive is complete")
+//                if (data != nil) {
+//                    let backToString = String(decoding: data!, as: UTF8.self)
+//                    print("Received message: \(backToString)")
+//                } else {
+//                    print("Data == nil")
+//                }
+//            }
+//        }
+//    }
+//}
