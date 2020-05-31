403e1a10f3c8635ada73e18d0323a0ff4494b52c
Cristian Iorga <ubik3000@gmail.com>
2020-05-31 11:02:50 +0300

    WoL: Added Network and CommandLine examples
    Added examples on how to send and receive UDP packets
    
    Added example usage for CommandLine

--- WOL/main.swift
+++ WOL/main.swift
@@ -7,6 +7,61 @@
 //
 
 import Foundation
+import Network
 
-print("Hello, World!")
 
+//Newer(??) alternative to CommandLine: https://swift.org/blog/argument-parser/
+//The Swift standard library contains a struct CommandLine which has a collection of Strings called arguments. So you could switch on arguments like this:
+//
+for argument in CommandLine.arguments {
+    switch argument {
+    case "arg1":
+        print("first argument")
+
+    case "arg2":
+        print("second argument")
+
+    default:
+        print("an argument")
+    }
+}
+
+var connection: NWConnection?
+
+func send() {
+    connection?.send(content: "Test message".data(using: String.Encoding.utf8), completion: NWConnection.SendCompletion.contentProcessed(({ (NWError) in
+        print(NWError!)
+    })))
+}
+
+func receive() {
+    connection?.receiveMessage { (data, context, isComplete, error) in
+        print("Got it")
+        print(data!)
+    }
+}
+
+func someFunc() {
+
+    connection = NWConnection(host: "255.255.255.255", port: 9093, using: .udp)
+
+    connection?.stateUpdateHandler = { (newState) in
+        switch (newState) {
+        case .ready:
+            print("ready")
+            send()
+            receive()
+        case .setup:
+            print("setup")
+        case .cancelled:
+            print("cancelled")
+        case .preparing:
+            print("Preparing")
+        default:
+            print("waiting or failed")
+
+        }
+    }
+    connection?.start(queue: .global())
+
+}
